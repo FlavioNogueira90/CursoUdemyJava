cba51a26e836fcbc0de8de1d83ef6fc293b5d340
Flavio Nogueira <flavionogueira20@gmail.com>
2021-07-23 12:16:54 -0300

    Projeto criado

---
+++ src/main/java/com/flavionogueira/artydapatty/ArtydapattyApplication.java
@@ -0,0 +1,13 @@
+package com.flavionogueira.artydapatty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtydapattyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtydapattyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flavionogueira/artydapatty/ArtydapattyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavionogueira.artydapatty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtydapattyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
