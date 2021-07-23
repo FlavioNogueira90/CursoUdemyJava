d11f3067d4951c8fd9e6c456c42f6aad02574077
Flavio Nogueira <flavionogueira20@gmail.com>
2021-07-23 17:47:53 -0300

    Criando operacao de instanciacao

--- src/main/java/com/flavionogueira/artydapatty/ArtydapattyApplication.java
+++ src/main/java/com/flavionogueira/artydapatty/ArtydapattyApplication.java
@@ -1,13 +1,32 @@
 package com.flavionogueira.artydapatty;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ArtydapattyApplication {
+import com.flavionogueira.artydapatty.domain.Categoria;
+import com.flavionogueira.artydapatty.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ArtydapattyApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ArtydapattyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
